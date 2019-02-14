d311977557f6b524c3a714555b990a88a6f567b2
izavits <izavits@gmail.com>
2019-02-14 09:43:49 +0200

    More tests

--- tests/dag_test.py
+++ tests/dag_test.py
@@ -4,14 +4,24 @@ import unittest
 from airflow.models import DagBag
 
 class TestDagIntegrity(unittest.TestCase):
-    """Test the DAG integrity."""
+    """Generic and DAG integrity tests."""
 
     LOAD_SECOND_THRESHOLD = 2
+
+    VALID_DAG_OWNERS = set([
+        'airflow',
+        ])
+
+    VALID_DAG_EMAILS = set([
+        'airflow@example.com',
+        ])
+    
 
     def setUp(self):
         self.dagbag = DagBag()
 
     def test_import_dags(self):
+        """Verify that Airflow will be abl to import the DAG."""
         self.assertFalse(
             len(self.dagbag.import_errors),
             'DAG import failures. Errors: {}'.format(
@@ -20,10 +30,44 @@ class TestDagIntegrity(unittest.TestCase):
         )
 
     def test_alert_email_present(self):
+        """Verify that an alert email is present."""
         for dag_id, dag in self.dagbag.dags.iteritems():
             emails = dag.default_args.get('email', [])
             msg = 'Alert email not set for DAG {id}'.format(id=dag_id)
             self.assertIn('airflow@example.com', emails, msg)
+
+    def test_dag_emails(self):
+        """Check that the DAG has a know email in the email list and a valid owner."""
+        for dag_id, dag in self.dagbag.dags.iteritems():
+            tasks_with_email_cnt = 0
+            for task in dag.tasks:
+                self._check_owner(task.owner, dag_id, task.task_id)
+                if task.email:
+                    self._check_email(task.email, dag_id, task.task_id)
+                    tasks_with_email_cnt += 1
+
+            if 'owner' in dag.default_args:
+                self._check_owner(dag.default_args.get('owner', False), dag_id)
+            if 'email' in dag.default_args:
+                self._check_email(dag.default_args.get('email', False), dag_id)
+
+            self.assertTrue(tasks_with_email_cnt == len(dag.tasks) or dag.default_args.get('email', False), \
+                'Either all tasks or default_args must have email set: {}'.format(dag_id))
+
+
+    def _check_owner(self, owner, dag_id, task_id=False):
+        # Check owner against valid owner list.
+        error_msg = 'DAG must have a valid owner ({}) defined: dag_id={}, task_id={}' \
+            .format(self.VALID_DAG_OWNERS, dag_id, task_id)
+        self.assertTrue(owner in self.VALID_DAG_OWNERS, error_msg)
+
+    def _check_email(self, email, dag_id, task_id=False):
+        # Airflow allows both a single email, as well as a list.
+        emails = email if isinstance(email, list) else [email]
+        error_msg = 'DAG must have a valid email ({}) defined: dag_id={}, task_id={}' \
+            .format(self.VALID_DAG_EMAILS, dag_id, task_id)
+        self.assertEqual(len(self.VALID_DAG_EMAILS.intersection(emails)), len(emails), error_msg)
+        
 
             
 class TestDAG(unittest.TestCase):
